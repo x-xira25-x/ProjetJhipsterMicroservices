ab04f698af5e589cb1f52def548edd11c1416b03
Hofmann Jenny <jenny.hofmann@he-arc.ch>
2018-06-08 11:34:55 +0200

    ajout authorité role admin pour bien etatbien type bien

--- AppliGateway/target/www/app/manifest.bundle.js
+++ AppliGateway/target/www/app/manifest.bundle.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "321b3d5596086a165edb"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "e51ce699c1219f454deb"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
