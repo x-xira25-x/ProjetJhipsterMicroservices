7e92552eaaa5fa35d180c074710369901b86a79d
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-06-20 16:18:58 +0200

    rajoute du user dans agent immo

--- AppliGateway/src/main/webapp/app/entities/agent-immobilier/agent-immobilier-dialog.component.ts
+++ AppliGateway/src/main/webapp/app/entities/agent-immobilier/agent-immobilier-dialog.component.ts
@@ -11,6 +11,7 @@ import { AgentImmobilierPopupService } from './agent-immobilier-popup.service';
 import { AgentImmobilierService } from './agent-immobilier.service';
 import {Principal, User, UserService} from '../../shared';
 import {Register} from '../../account';
+import {Client} from "../client";
 
 @Component({
     selector: 'jhi-agent-immobilier-dialog',
@@ -21,7 +22,7 @@ export class AgentImmobilierDialogComponent implements OnInit {
     agentImmobilier: AgentImmobilier;
     isSaving: boolean;
     registerAccount: any;
-    users: User[];
+    usersAll: User[];
     success: boolean;
     confirmPassword: string;
     doNotMatch: string;
@@ -30,6 +31,8 @@ export class AgentImmobilierDialogComponent implements OnInit {
     errorUserExists: string;
     user: User;
     authorities: any[];
+    users: User[];
+    incremal: number;
 
     constructor(
         public activeModal: NgbActiveModal,
@@ -47,18 +50,45 @@ export class AgentImmobilierDialogComponent implements OnInit {
         this.authorities = [];
         this.registerAccount = {};
         this.userService.query()
-            .subscribe((res: HttpResponse<User[]>) => { this.users = res.body; }, (res: HttpErrorResponse) => this.onError(res.message));
+            .subscribe((res: HttpResponse<User[]>) => {
+                this.users = res.body;
+             /*   this.incremal= 0;
+                // récupérer dans la liste que les users avec une authorité d'agent
+                for( let i =0; i<= this.usersAll.length; i++ ){
+                    if(this.usersAll[i].authorities[1] == "ROLE_AGENTIMMO"){
+                        console.log(this.incremal)
+                        this.users[this.incremal] = this.usersAll[i];
+                        this.incremal ++;
+                    }
+
+                }*/
+                console.log(this.users)
+                }, (res: HttpErrorResponse) => this.onError(res.message));
         this.userService.authorities().subscribe((authorities) => {
             this.authorities = authorities;
 
         });
-        this.principal.identity().then((account) => {
+
+      /*  console.log(this.agentImmobilier)
+        if(this.agentImmobilier.idUser !== undefined){
+            this.userService.findUserById(this.agentImmobilier.idUser).subscribe(resp => {
+            this.user = resp.body;
+            console.log(this.user);
+            this.copyAccount(this.user);
+            console.log(this.copyAccount(this.user))
+        });
+        }else{
+            this.user = {};
+            console.log(this.user)
+        }*/
+
+    /*    this.principal.identity().then((account) => {
             // par rapport au login avoir l'id du user
-            console.log(this.copyAccount(account).login);
+            console.log("ngini" + this.copyAccount(account).login);
             this.userService.find(this.copyAccount(account).login).subscribe(resp => {
                 this.user = resp.body;
             });
-        });
+        });*/
     }
 
     clear() {
@@ -67,31 +97,41 @@ export class AgentImmobilierDialogComponent implements OnInit {
 
     save() {
         this.isSaving = true;
+        this.userService.findUserById(this.agentImmobilier.idUser).subscribe(resp => {
+            this.user = resp.body;
+            this.agentImmobilier.email= this.user.email;
+        })
         if (this.agentImmobilier.id !== undefined) {
+            console.log(this.agentImmobilier)
+
             this.subscribeToSaveResponse(
                 this.agentImmobilierService.update(this.agentImmobilier));
         } else {
-            if (this.registerAccount.password !== this.confirmPassword) {
+           /* if (this.registerAccount.password !== this.confirmPassword) {
                 this.doNotMatch = 'ERROR';
             } else {
+
                 this.doNotMatch = null;
                 this.error = null;
                 this.errorUserExists = null;
                 this.errorEmailExists = null;
                 this.registerAccount.langKey = 'en';
+                console.log(this.registerAccount)
                 this.registerService.save(this.registerAccount).subscribe(() => {
-                    this.success = true;
-                    this.userService.find(this.registerAccount.login).subscribe(resp => {
+                    this.success = true;*/
+                    /*this.userService.find(this.registerAccount.login).subscribe(resp => {
                         this.user = resp.body;
                         this.user.authorities = this.authorities;
                         this.userService.update(this.user).subscribe();
                         this.agentImmobilier.email = this.registerAccount.email;
                         this.agentImmobilier.idUser = this.user.id;
+                        console.log(this.agentImmobilier)*/
+                    console.log(this.agentImmobilier)
                         this.subscribeToSaveResponse(
                             this.agentImmobilierService.create(this.agentImmobilier));
-                    });
-                });
-            }
+                    /*});*/
+              /*  });*/
+            /*}*/
         }
     }
 
@@ -111,6 +151,10 @@ export class AgentImmobilierDialogComponent implements OnInit {
     }
     private onError(error: any) {
         this.jhiAlertService.error(error.message, null, null);
+    }
+
+    trackUserById(index:number, item: User) {
+        return item.id;
     }
 
     copyAccount(account) {
