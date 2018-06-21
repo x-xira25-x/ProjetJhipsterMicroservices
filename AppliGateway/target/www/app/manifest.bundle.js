1b31d0f2a69d2c43afcb67cc0064a79d6d6d993f
x-xira25-x <32196605+x-xira25-x@users.noreply.github.com>
2018-06-21 09:11:58 +0200

    mise en page de la description dans les card  pour bien à vendre

--- AppliGateway/target/www/app/manifest.bundle.js
+++ AppliGateway/target/www/app/manifest.bundle.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "76d25552050e651e270c"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "f20a8ee9de7cbad5af53"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
